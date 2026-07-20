d9a44047b6b2380e8635f8c2184e8d0bf8ed5704
Meldrid <maarijmoeen12345@gmail.com>
2026-07-20 16:52:33 +0500

    Solve sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // validating the inputs
+    // check if they are numbers or not
+    if (Number.isNaN(num1) || Number.isNaN(num2))
+        return "ERROR"
+    // negative inputs
+    else if (num1 < 0 || num2 < 0)
+        return "ERROR"
+    // checking if decimal or not
+    else if (Math.floor(num1) !== num1 || Math.floor(num2) !== num2)
+        return "ERROR"
+    // fixing the order
+    else if (num1 > num2) {
+        let temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    let result = 0;
+    for (let i = num1; i <= num2; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
