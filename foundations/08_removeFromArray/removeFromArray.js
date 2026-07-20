ccd0687898d1355359c582b52382e2952bc0fffa
Meldrid <maarijmoeen12345@gmail.com>
2026-07-20 16:41:47 +0500

    Solve removeElement exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...targets) {
+    for (target of targets) {
+        array = array.filter((element) => {
+            return element !== target;
+        })
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
