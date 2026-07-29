206c883ebce3ec1aff4ad46fe7bd6411f0152f65
Maarij Moeen <maarijmoeen12345@gmail.com>
2026-07-29 21:21:38 +0500

    Solve exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    // check the yearOfDeath for first person
+    let firstPerson = people[0];
+    if (firstPerson.yearOfDeath === undefined)
+        firstPerson.yearOfDeath = new Date().getFullYear();
+    const array = people.reduce((accumulated, current) => {
+        // the person at the last index represents the oldest person
+        // if current last index is not older than current, push current to end
+        const lastIndex = accumulated.length - 1;
+        let currentDeathYear = current.yearOfDeath;
+        if (currentDeathYear === undefined)
+            currentDeathYear = new Date().getFullYear();
+        if (currentDeathYear - current.yearOfBirth > accumulated[lastIndex].yearOfDeath - accumulated[lastIndex].yearOfBirth)
+            accumulated.push(current);
+        return accumulated;
+    }, [firstPerson])
+    // return the oldest person
+    return array[array.length - 1];
 };
 
 // Do not edit below this line
