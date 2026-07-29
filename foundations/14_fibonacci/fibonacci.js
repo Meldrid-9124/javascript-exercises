38536086c22bc69bd1dc061b71597863301857c8
Maarij Moeen <maarijmoeen12345@gmail.com>
2026-07-29 19:24:08 +0500

    Solve exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    // handle the case of negatives
+    if (num < 0)
+        return "OOPS";
+    // start with 0 and 1
+    let sequence = [0, 1];
+    for (let i = 0; i < num; i++) {
+        const lastIndex = sequence.length - 1;
+        // insert the sum of current last and second last elements of the sequence
+        sequence.push(sequence[lastIndex] + sequence[lastIndex - 1]);
+    }
+    return sequence[num];
 };
 
 // Do not edit below this line
